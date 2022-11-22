0878f8094ede18423eadb5a55e6b03d37e7a5956
offiahchiaz <offiahchiaz@gmail.com>
2022-11-22 03:37:36 +0100

    Updated App with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-//import SignUp from './pages/SignUp/SignUp';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import SignUp from './pages/signup/Signup';
 import Login from './pages/login/Login';
 
 function App() {
  
   return (
     <div className="">
-      {/* <SignUp /> */}
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <SignUp />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <SignUp />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
